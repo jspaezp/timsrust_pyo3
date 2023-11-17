2f96effb05722d6a4c39c053fe76fdf570bc6fc1
J. Sebastian Paez <jspaezp@gmail.com>
2023-11-16 17:54:20 -0800

    (wip) transitioning to 0.2.2

--- src/lib.rs
+++ src/lib.rs
@@ -3,28 +3,29 @@ use std::collections::HashMap;
 use crate::timsrust::Spectrum;
 use crate::timsrust::Frame;
 use crate::timsrust::FrameType;
-use pyo3::ffi::PyPreConfig_InitIsolatedConfig;
-//use crate::timsrust::converters::{Frame2RtConverter, Tof2MzConverter, Scan2ImConverter};
+use crate::timsrust::ConvertableIndex;
 use pyo3::prelude::*;
 use pyo3::types::IntoPyDict;
 use timsrust;
 use timsrust::AcquisitionType;
-use timsrust::ConvertableIndex;
-use timsrust::Precursor;
-use timsrust::PrecursorType;
-use timsrust::ReadableFrames;
+use timsrust::QuadrupoleEvent;
 
 #[pyclass]
-struct TDFReader {
-    pub reader: timsrust::TDFReader,
+struct TimsReader {
+    pub path: String,
+    pub reader: timsrust::FileReader,
 }
 
 #[pymethods]
-impl TDFReader {
+impl TimsReader {
     #[new]
-    fn new(path: String) -> Self {
-        TDFReader {
-            reader: timsrust::TDFReader::new(&path),
+    fn new(path: String) -> PyResult<Self> {
+        use pyo3::exceptions::PyIOError;
+        let reader = timsrust::FileReader::new(&path);
+        match reader {
+            Ok(x) => Ok(TimsReader { reader: x, path: path }),
+            Err(x) => Err(PyIOError::new_err("Could not open file")),
+            
         }
     }
     fn read_all_frames(&self) -> Vec<PyFrame> {
@@ -45,8 +46,9 @@ impl TDFReader {
 
     fn read_dia_frames(&self) -> Vec<PyFrame> {
         self.reader
-            .read_all_dia_frames()
+            .read_all_frames()
             .iter()
+            .filter(|x| x.frame_type == FrameType::MS2(AcquisitionType::DIAPASEF))
             .map(|x| PyFrame::new(x))
             .collect()
     }
@@ -59,54 +61,57 @@ impl TDFReader {
             .collect()
     }
 
-    fn dia_frame_table(&self) -> HashMap<String, Vec<usize>> {
-        let frametable = &self.reader.dia_frame_table;
-        let mut frametable_out: HashMap<String, Vec<usize>> = HashMap::new();
+    // TODO implement on the python end
+    // fn dia_frame_table(&self) -> HashMap<String, Vec<usize>> {
+    //     let frametable = &self.reader.dia_frame_table;
+    //     let mut frametable_out: HashMap<String, Vec<usize>> = HashMap::new();
 
-        frametable_out.insert("frame".to_string(), frametable.frame.to_owned());
-        frametable_out.insert("group".to_string(), frametable.group.to_owned());
+    //     frametable_out.insert("frame".to_string(), frametable.frame.to_owned());
+    //     frametable_out.insert("group".to_string(), frametable.group.to_owned());
 
-        frametable_out
-    }
+    //     frametable_out
+    // }
 
-    fn dia_frame_msms_windows(&self, py: Python) -> PyResult<PyObject> {
-        let msms_frame_window_table = &self.reader.dia_frame_msms_table;
+    // fn dia_frame_msms_windows(&self, py: Python) -> PyResult<PyObject> {
+    //     let msms_frame_window_table = &self.reader.dia_frame_msms_table;
 
-        let key_vals: &[(&str, PyObject)] = &[
-            (
-                "group",
-                msms_frame_window_table.group.to_owned().to_object(py),
-            ),
-            (
-                "scan_start",
-                msms_frame_window_table.scan_start.to_owned().to_object(py),
-            ),
-            (
-                "scan_end",
-                msms_frame_window_table.scan_end.to_owned().to_object(py),
-            ),
-            (
-                "mz_center",
-                msms_frame_window_table.mz_center.to_owned().to_object(py),
-            ),
-            (
-                "mz_width",
-                msms_frame_window_table.mz_width.to_owned().to_object(py),
-            ),
-        ];
+    //     let key_vals: &[(&str, PyObject)] = &[
+    //         (
+    //             "group",
+    //             msms_frame_window_table.group.to_owned().to_object(py),
+    //         ),
+    //         (
+    //             "scan_start",
+    //             msms_frame_window_table.scan_start.to_owned().to_object(py),
+    //         ),
+    //         (
+    //             "scan_end",
+    //             msms_frame_window_table.scan_end.to_owned().to_object(py),
+    //         ),
+    //         (
+    //             "mz_center",
+    //             msms_frame_window_table.mz_center.to_owned().to_object(py),
+    //         ),
+    //         (
+    //             "mz_width",
+    //             msms_frame_window_table.mz_width.to_owned().to_object(py),
+    //         ),
+    //     ];
 
-        let dict = key_vals.into_py_dict(py);
-        Ok(dict.into())
-    }
+    //     let dict = key_vals.into_py_dict(py);
+    //     Ok(dict.into())
+    // }
 
     fn __repr__(slf: &PyCell<Self>) -> PyResult<String> {
         let class_name: &str = slf.get_type().name()?;
-        Ok(format!("{}(path={})", class_name, slf.borrow().reader.path))
+        Ok(format!("{}(path=???)", class_name))
     }
     fn resolve_mzs(slf: &PyCell<Self>, tofs: Vec<u32>) -> Vec<f64> {
-        tofs.iter()
-            .map(|tof| slf.borrow().reader.mz_converter.convert(*tof))
-            .collect()
+        let converter = slf.borrow()
+            .reader
+            .get_tof_converter().unwrap();
+        
+        tofs.iter().map(|x| converter.convert(*x)).collect()
     }
 }
 
@@ -144,8 +149,8 @@ impl PyPrecursor {
 impl PySpectrum {
     fn new(scan: &Spectrum) -> Self {
         let precursor = match scan.precursor {
-            PrecursorType::Precursor(x) => PyPrecursor::new(&x),
-            PrecursorType::None => PyPrecursor {
+            QuadrupoleEvent::Precursor(x) => PyPrecursor::new(&x),
+            QuadrupoleEvent::None => PyPrecursor {
                 mz: 0.0,
                 im: 0.0,
                 charge: 0,
@@ -235,13 +240,13 @@ impl PyFrame {
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn read_all_frames(a: String) -> PyResult<Vec<PyFrame>> {
-    let fr = timsrust::TDFReader::new(&a);
+    let fr = timsrust::FileReader::new(&a);
+    let fr = TimsReader {
+        reader: fr.unwrap(),
+        path: a,
+    };
 
-    let out: Vec<PyFrame> = fr
-        .read_all_frames()
-        .iter()
-        .map(|x| PyFrame::new(x))
-        .collect();
+    let out: Vec<PyFrame> = fr.read_all_frames();
     Ok(out)
 }
 
@@ -249,7 +254,7 @@ fn read_all_frames(a: String) -> PyResult<Vec<PyFrame>> {
 #[pymodule]
 fn timsrust_pyo3(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(read_all_frames, m)?)?;
-    m.add_class::<TDFReader>()?;
+    m.add_class::<TimsReader>()?;
     m.add_class::<PyFrame>()?;
     Ok(())
 }
